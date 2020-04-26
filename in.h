aa663a1f4dd0b2852a477e31e9da83ed9524412e
Drus <verssy@mail.ru>
2020-04-26 18:16:28 +0700

    Added header file in.h for in.cpp

---
+++ in.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef IN_H
+#define IN_H
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+void read(std::vector<std::vector<int>>& data);
+
+#endif 
